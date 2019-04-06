f02eaa9a1efcd842ec5e538fb912fafe6047c233
Shane D Wilson <me@shanedwilson.com>
2019-04-06 11:54:40 -0500

    finished tests

--- BadSuperbowlNamer3/RomanToNumberConvertor.cs
+++ BadSuperbowlNamer3/RomanToNumberConvertor.cs
@@ -9,15 +9,10 @@ namespace BadSuperbowlNamer3
         Dictionary<string, int> allNumbers = new Dictionary<string, int>()
         {
             { "M", 1000},
-            { "CM", 900 },
             { "D", 500 },
-            { "CD", 400 },
             { "C", 100 },
-            { "XC", 90  },
             { "L", 50 },
-            { "XL", 40},
             { "X", 10 },
-            { "IX", 9 },
             { "V", 5 },
             { "I", 1 },
         };
